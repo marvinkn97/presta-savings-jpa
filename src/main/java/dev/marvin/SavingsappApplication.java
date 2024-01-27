bd0b40e6fc5d514d2b86d65277624495cc0cdbe6
marvinkn <marvin.nyingi97@gmail.com>
2024-01-27 12:50:06 +0300

    getting started

---
+++ src/main/java/dev/marvin/SavingsappApplication.java
@@ -0,0 +1,13 @@
+package dev.marvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingsappApplication.class, args);
+	}
+
+}
