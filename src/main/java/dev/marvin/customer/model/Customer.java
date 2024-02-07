43d8aaa9af74040677faa3378dcb2193f256be65
marvinkn <marvin.nyingi97@gmail.com>
2024-02-07 15:54:32 +0300

    model -> Customer

--- src/main/java/dev/marvin/customer/model/Customer.java
+++ src/main/java/dev/marvin/customer/model/Customer.java
@@ -1,12 +1,40 @@
 package dev.marvin.customer.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="t_customer")
+@Builder
+@Data
 public class Customer {
-    private Integer customerId;
-    private String customerName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(updatable = false)
+    private Integer id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String mobile;
+    private Integer governmentId;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+    private String profilePic;
 
 }
